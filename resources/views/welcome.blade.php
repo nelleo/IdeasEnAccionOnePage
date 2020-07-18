fbb941593ffe6a040b39dc738aeef38d6d705e1a
nelleo <gustavoLCS.271@gmail.com>
2020-07-18 20:38:56 -0300

    inicio de proyecto ,maquetacion

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ideas En Acción</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Monserrat:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css')}}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid sinpadding" style="background-color: #062a50">  
+            <div class="row">
+                <div class="col-md-3 col-lg-3 mobile"> 
+                    <img class="imgmh col-lg-12" src="images/img--01.jpg" alt="">
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 " >
+                    <img class="titulo" src="images/img--02.jpg" alt="titulo pricipal">
+                    <div class="seccion1">
+                        <img  class="" src="images/img--03.jpg" alt="">
+                        <p class="subtitulo" style="color: aliceblue"><strong>TÚ INNOVACIÓN Y/O DESAFÍOS <br> CUENTAN</strong></p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-lg-3 mobile">
+                    <img class="imgmh col-lg-12" src="images/img--04.jpg" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 seccion2" style="text-align: center">
+                    <img src="images/img--05.jpg" alt="">
+                    <p class="parrafo1" style="color: aliceblue">La convocatoria abierta del <strong>Premio Ideas en <br> Acción </strong>invita a presentar innovaciones <br>
+                    implementadas por <strong>empresas prestadoras de <br> servicio de agua y/o saneamiento en América <br>
+                    Latina y el Caribe </strong><br><br>
+                    Este año , la convocatoria también invita a las <br>empresas a presentar desafíos o problemáticas
+                    <br> que puedan ser atendidas a través de soluciones <br>innovadoras en desalinización ,tratamiento y <br>
+                    reúso, SWIT y/o gestión. <br><br>
+                    En el 2020 el <strong>Premio Ideas en Acción</strong> consta <br>de dos categorías</p>
+                    <img src="images/img--06.jpg" alt="">
+                </div>
+                <div class="col-6 mobile">
+                    <img src="images/img--07.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
