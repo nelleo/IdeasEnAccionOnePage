1fa3ad872ac81ace1f6327d739c2aac8d9c8e1be
nelleo <gustavoLCS.271@gmail.com>
2020-07-20 08:04:03 -0300

    podrasGanar,form,footer ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,9 +39,9 @@
             <div class="col-lg-6 p-0">
                 <div class="d-flex flex-column flex-lg-row w-100 tlb">
                     <p class="p-4" style="color: aliceblue">
-                        La convocatoria abierta del Premio Ideas en Acción invita a presentar innovaciones implementadas por empresas prestadoras de servicio de agua y/o saneamiento en América Latina y el Caribe. 
-                        <br>Este año, la convocatoria también invita a las empresas a presentar desafíos o problemáticas que puedan ser atendidas a través de soluciones innovadoras en desalinización, tratamiento y reúso, SWIT y/o gestión.
-                        <br>En el 2020 el Premio Ideas en Acción consta de dos categorías.
+                        La convocatoria abierta del <strong> Premio Ideas en Acción </strong>invita a presentar innovaciones implementadas por <strong>empresas prestadoras de servicio de agua y/o saneamiento en América Latina y el Caribe.</strong> 
+                        <br><br>Este año, la convocatoria también invita a las empresas a presentar desafíos o problemáticas que puedan ser atendidas a través de soluciones innovadoras en desalinización, tratamiento y reúso, SWIT y/o gestión.
+                        <br><br>En el 2020 el <strong>Premio Ideas en Acción </strong>consta de dos categorías.
                     </p>
                 </div>
                 <div>
@@ -53,44 +53,101 @@
             </div>
                 
         </section>
-        <div class="row">
-            <form class="mx-auto mt-4 mb-5 text-center" method="POST" action="/envioMail">
-                @csrf
-                <div class="form-row mb-3 mw-100">
-                  <div class="form-group col-md-4 mt-2" >
-                    <input type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus  placeholder="Nombre" >
-                    @error('nombre')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                  </div>
-                  <div class="form-group col-md-4 mt-2">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Dirección de correro electrónico">
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                  </div>
+        <section style="background-color:#ecfcf4 ">
+            <div>
+
+           </div>
+           <div>
+
+           </div>
+           <div>
+
+           </div>
+        </section>
+        <section class="text-center sp" style="background-color:#24959e ">
+            <div class="row ">
+                <div class="offset-3 col-md-5 mt-5">
+                    <h2 style="text-decoration: underline;line-height: 1.2em;color: aliceblue">Podrás ganar:</h2>
+                    <p style="color: aliceblue">Asistencia técnica para asesorías,<br>
+                    estudios de prefactibilidad / factibilidad,<br>
+                    planes de negocios, y capacitación,<br>
+                    así como la implementación del proyecto piloto <br>
+                    enfocado a resolver el desafío formulado.</p>
                 </div>
-                <div class="form-row mb-3 mw-100">
-                    <div class="form-group col-md-4 mt-2">
-                      <textarea class="form-control @error('mensaje') is-invalid @enderror radius" name="mensaje"  value="{{ old('mensaje') }}" id="" cols="30" rows="5" required autocomplete="mensaje" autofocus placeholder="Mensaje"></textarea>                              
-                      @error('mensaje')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
+                <div class="offset-1  col-md-2">
+                    <img class="w1" src="images/img--32.jpg" alt="">
+                </div>
+            </div>
+        </section>
+        <img src="images/img--33.jpg" alt="olas" class="w1">
+        <section class="d-flex flex-wrap w-100 marino flex-lg-row oh">
+            <div class="col-md-6 col-lg-6 text-center mt-3" style="color: aliceblue">
+                <h2 class="col-md-12 mb-4" style="font-weight: 700;font-size:xx-large!important; letter-spacing:0.1em!important;" >Contáctenos</h2>  
+                <div class="">
+                    <p style="">¿Tiene preguntas?<br>Contactanos.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <form class="mr-5 mt-4 mb-5 text-right" method="POST" action="/envioMail" >
+                    @csrf
+                    <div class="form-row mb-1 mw-100">
+                      <div class="form-group col-md-4 " >
+                        <input type="text" class="form-control @error('nombre') is-invalid @enderror marino" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus  placeholder="Nombre">
+                        @error('nombre')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                      </div>
+                      <div class="form-group col-md-8">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror marino" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Dirección de correro electrónico" >
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                      </div>
+                    </div>
+                    <div class="form-row mb-3 mw-100">
+                        <div class="form-group col-md-12 mt-2">
+                          <textarea class="form-control @error('mensaje') is-invalid @enderror radius marino" name="mensaje"  value="{{ old('mensaje') }}" id="" cols="30" rows="5" required autocomplete="mensaje" autofocus placeholder="Mensaje" ></textarea>                              
+                          @error('mensaje')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        </div>
+                    </div>
+                      @if(Session::has('mensaje'))
+                        <div class="alert alert-success mt-1 mb-1" role="alert">
+                          {{ Session::get('mensaje')}} 
+                        </div>
+                      @endif
+                      <button type="submit" class="btn ba mr-3" style="background-color:#eafaf5;color:#061939;border:none;font-weight: 500;">ENVIAR</button>
+                  </form>
+            </div>
+        </section>
+        <footer class="row sp">
+            <div class="d-none d-sm-block col-md-4">
+                <img class="w1 abajo" src="images/img--34.jpg" alt="img footer">
+            </div>
+            <div class="col-md-4 ">
+                <div class="row ">
+                    <div class="col-md-6 ">
+                        <img class="w1" src="images/img--35.jpg" alt="img footer">
+                    </div>
+                    <div class="col-md-6 ">
+                        <img class="w1" src="images/img--36.jpg" alt="img footer">
+                    </div>
+                    <div class="col-md-12 foot-ctrl">
+                        <img class="w1 " src="images/img--37.jpg" alt="img footer" >
+                        {{-- style="visibility: hidden;" --}}
                     </div>
                 </div>
-                  @if(Session::has('mensaje'))
-                    <div class="alert alert-success mt-1 mb-1" role="alert">
-                      {{ Session::get('mensaje')}} 
-                    </div>
-                  @endif
-                  <button type="submit" class="btn ba" style="background-color:#1c4c84;color:#f7ca48;border:none;font-weight: 800;">ENVIAR</button>
-              </form>
-        
+            </div>
+            <div class="d-none d-sm-block col-md-4 ">
+                <img class="w1 abajo" src="images/img--38.jpg" alt="img footer">
+            </div>
+        </footer>
     </body>
 </html>
