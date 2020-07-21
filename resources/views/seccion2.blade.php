a22366a8147d6b2cf10391a1ee7fe0170219904f
Ricardo Daniel Romero <Richard@Ricardos-MacBook-Pro.local>
2020-07-21 14:50:17 -0300

    R4

--- resources/views/seccion2.blade.php
+++ resources/views/seccion2.blade.php
@@ -2,24 +2,18 @@
     <h1 class="cat">Categorías</h1>
     <img src="images/img--08.jpg" alt="olas">
 </div>
-<section class="d-flex mt-5" style="justify-content: center">
+<section class="d-flex mt-5 justify-content-around flex-wrap" style="justify-content: center">
 
-    <div class="text-center mr-5" >
-        <img class="" src="images/img--09.jpg" alt="">
-        <p style="color:#ecfcf4;font-size:large; ">Para <strong>innovaciones implementadas</strong> por empresas <br>
-            prestadoras de servicios de agua y/o saneamiento en <br>
-            América Latina y el Caribe.</p>
+    <div class="text-center" >
+        <img class="mw-100" src="images/img--09.jpg" alt="titulo excelencia innovadora empresarial">
+        <p class="p-4" style="max-width: 500px;color:#ecfcf4;font-size:large; ">Para <strong>innovaciones implementadas</strong> por empresas prestadoras de servicios de agua y/o saneamiento en América Latina y el Caribe.</p>
         <h3 class="part1">PARTICIPA</h3>
         <img src="images/img--10.jpg" alt="">
     </div>
 
     <div class="text-center">
-        <img src="images/img--12.jpg" alt="">
-        <p style="color:#ecfcf4;font-size:large;">Para empresas prestadoras de servicios de agua y/o <br>
-            saneamiento en América Latina y el Caribe con <br>
-            <strong>desafíos</strong> que puedan ser solucionados a través de la <br>
-            implementación de pilotos innovadores en <br>
-            tecnologías inteligentes, desalinización, reúso y/o gestión.</p>
+        <img class="mw-100" src="images/img--12.jpg" alt="titulo desafíos">
+        <p class="p-4" style="max-width: 500px;color:#ecfcf4;font-size:large;">Para empresas prestadoras de servicios de agua y/o saneamiento en América Latina y el Caribe con <strong>desafíos</strong> que puedan ser solucionados a través de la implementación de pilotos innovadores en tecnologías inteligentes, desalinización, reúso y/o gestión.</p>
         <h3 class="part2">PARTICIPA</h3>
         <img src="images/img--13.jpg" alt="">
     </div>
@@ -27,5 +21,5 @@
 </section>
 <div class="d-flex justify-content-center flex-column">
     <h1 class="dqst">¿De qué se trata?</h1> 
-    <img src="images/img--14-f.jpg" alt="olas" class="w1 ">
+    <img src="images/img--14-f.jpg" alt="fondo ¿De qué trata?">
 </div>
--- resources/views/seccion3.blade.php
+++ resources/views/seccion3.blade.php
@@ -1,19 +1,18 @@
-<section class="row w1" style="background-color:#04b3bb;margin:0%;">
+<section class="d-flex " style="background-color:#00b1b8;margin:0%;">
     <div class="col-md-2 footer">
         <img class="" src="images/img--15.jpg" alt="misc titulo 1">
         <img class="abajo" src="images/img--21.jpg" alt="misc titulo 1">
     </div>
-    <div class="col-md-8 text-center" style="color: #ecfcf4;">
+    <div class=" text-center" style="color: #ecfcf4;">
         <div class="row">
-            <h5 class="col-md-8 offset-2  mt-4" style="font-weight: 400;
-            letter-spacing: 0.2em;">CATEGORÍA</h5>
-            <h1 class="col-md-10 offset-1 mt-4" style="font-weight: 500!important;">Excelencia Innovadora Empresarial</h1>
-            <p class="col-md-10 offset-1 mt-4 w1" style=" font-size: large;font-weight: 400;">Busca iniciativas, prácticas y modelos innovadores creados y/o utilizados para <br>
+            <h5 class="mt-4" style="font-weight: 400;letter-spacing: 0.2em;">CATEGORÍA</h5>
+            <h1 class="mt-4" style="font-weight: 500!important;">Excelencia Innovadora Empresarial</h1>
+            <p class="mt-4" style=" font-size: large;font-weight: 400;">Busca iniciativas, prácticas y modelos innovadores creados y/o utilizados para <br>
                 afrontar retos que tienen las empresas prestadoras de servicios de agua y/o <br>
                 saneamiento en América Latina y el Caribe, que hayan sido probados e <br>
                 implementados; y que hayan generado un impacto positivo en alguna de las <br>
                 siguientes categorías:</p>
-            <div class="col-md-10 offset-1 mr-5 mt-4 d-flex" style="justify-content: center">
+            <div class="mr-5 mt-4 d-flex flex-wrap">
 
                 <div><img class="" src="images/img--18.jpg" alt="misc titulo 1">
                     <p style=" font-size: large;"><strong>Innovación <br>
@@ -31,12 +30,12 @@
                 </div>
 
             </div>
-            <p class="col-md-10 offset-1 mt-4" style=" font-size: large;font-weight: 400;">Si tu empresa ha implementado un programa, práctica, producto, o servicio <br>
+            <p class=" mt-4" style=" font-size: large;font-weight: 400;">Si tu empresa ha implementado un programa, práctica, producto, o servicio <br>
                 innovador en el área ambiental, social, tecnológica o procedimental enfocado <br>
                 en AGUA y/o SANEAMIENTO <strong> ¡esta categoría es para ti!</strong> </p>
-            <div class="col-md-6 offset-3 text-center">
-                <button class="col-md-10 mt-4  btn" style="background-color:#ecfcf4;color:#044363;font-weight: 500; ">ENVÍA TU PROPUESTA</button>
-                <button class="col-md-10 mt-4 mb-5 btn" style="background-color:#044363;color:#ecfcf4;font-size: small; ">VER BASES Y CONDICIONES DE ESTA CATEGORÍA</button>
+            <div class=" text-center">
+                <button class="mt-4 btn" style="background-color:#ecfcf4;color:#044363;font-weight: 500; ">ENVÍA TU PROPUESTA</button>
+                <button class="mt-4 mb-5 btn" style="background-color:#044363;color:#ecfcf4;font-size: small; ">VER BASES Y CONDICIONES DE ESTA CATEGORÍA</button>
             </div>
         </div>
     </div>
