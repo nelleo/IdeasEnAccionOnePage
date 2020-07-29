1ea16d83089124441ecfce346b1676a082e9189f
eotmb <agustinmorelli88@gmail.com>
2020-07-29 01:44:25 -0300

    Fix font

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -7,7 +7,7 @@
         <title>@lang('menssages.title')</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Monserrat:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
